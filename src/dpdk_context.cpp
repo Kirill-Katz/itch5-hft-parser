97596564c9fdb6b4ff2199b1011abf62fa7bc187
Kirill Katz <xnlyre@protonmail.com>
2026-05-04 12:34:35 +0300

    Refactor a bit

--- src/dpdk_context.cpp
+++ src/dpdk_context.cpp
@@ -19,10 +19,13 @@ void DPDKContext::setup_eal(int& argc, char**& argv) {
 }
 
 void DPDKContext::setup_mempool() {
+    constexpr uint32_t mbuf_count = 8191;
+    constexpr uint32_t mbuf_cache_size = 256;
+
     pool_ = rte_pktmbuf_pool_create(
         "mbuf_pool",
-        1024,
-        256,
+        mbuf_count,
+        mbuf_cache_size,
         0,
         RTE_PKTMBUF_HEADROOM + 2048,
         rte_socket_id()
@@ -39,6 +42,11 @@ void DPDKContext::setup_eth_device(uint16_t port_id) {
     if (rte_eth_dev_info_get(port_id, &dev_info) != 0)
         throw std::runtime_error("dev info failed");
 
+    uint16_t rx_desc = 1024;
+    uint16_t tx_desc = 1024;
+    if (rte_eth_dev_adjust_nb_rx_tx_desc(port_id, &rx_desc, &tx_desc) != 0)
+        throw std::runtime_error("adjust nb desc failed");
+
     rte_eth_conf conf{};
     conf.txmode.offloads = 0;
     conf.rxmode.offloads = 0;
@@ -52,11 +60,11 @@ void DPDKContext::setup_eth_device(uint16_t port_id) {
     rte_eth_rxconf rxconf = dev_info.default_rxconf;
     rxconf.offloads = 0;
 
-    if (rte_eth_tx_queue_setup(port_id, 0, 1024,
+    if (rte_eth_tx_queue_setup(port_id, 0, tx_desc,
                                rte_socket_id(), &txconf) != 0)
         throw std::runtime_error("tx queue failed");
 
-    if (rte_eth_rx_queue_setup(port_id, 0, 1024,
+    if (rte_eth_rx_queue_setup(port_id, 0, rx_desc,
                                rte_socket_id(), &rxconf, pool_) != 0)
         throw std::runtime_error("rx queue failed");
 
